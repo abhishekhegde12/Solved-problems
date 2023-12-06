89b226aa409871e847e0e0dc9e269f908c17c4ba
Abhishek Hegde <abhishekhegde122002@gmail.com>
2023-12-06 18:51:23 +0530

    Add files via upload

---
+++ newMonday.java
@@ -0,0 +1,51 @@
+// Calculate Money in Leetcode Bank
+
+// CODE:
+
+class newMonday {
+    public int totalMoney(int n) {
+        int total=0,j=1;
+        int count=1;
+        if(n<=7)
+            return n*(n+1)/2;
+        for(int i=1;i<=n;i++){
+            if(i%7==0){
+                total+=j;
+                j=count++;
+            }
+            else
+                total+=j;
+            j++;
+        }
+        return total;
+    }
+    public static void main(String[] args) {
+        System.out.println("Total money is "+new newMonday().totalMoney(5));
+    }
+}
+
+
+// DISCRIPTION:
+
+// Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
+
+// He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
+// Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
+
+ 
+
+// Example 1:
+
+// Input: n = 4
+// Output: 10
+// Explanation: After the 4th day, the total is 1 + 2 + 3 + 4 = 10.
+// Example 2:
+
+// Input: n = 10
+// Output: 37
+// Explanation: After the 10th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37. Notice that on the 2nd Monday, Hercy only puts in $2.
+// Example 3:
+
+// Input: n = 20
+// Output: 96
+// Explanation: After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96.
