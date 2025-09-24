04b979dd0cf462d5d4c25e4296c8f3c39f4b855a
Abhishek Hegde <abhishekhegde122002@gmail.com>
2025-09-24 22:25:36 +0530

    permutations.java_1

--- Permutations.java
+++ Permutations.java
@@ -1,10 +1,27 @@
 import java.util.*;
 
 public class Permutations {
-
+ 
+    private static void printPermutation(int n) {
+        if(n<=3) {
+            System.out.println("NO SOLUTION");
+            return;
+        }
+        for(int i=1; i<=n; i++) {
+            if(i%2==0)
+                System.out.print(" "+i);
+        }
+        for(int i=1; i<=n; i++) {
+            if(i%2!=0)
+                System.out.print(" "+i);
+        }
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+    
         int n = scan.nextInt();
         scan.close();
+        
+        printPermutation(n);
     }
 }
