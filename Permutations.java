8e71b86f043221289b2e8c5989d0ce310af476d3
Abhishek Hegde <abhishekhegde122002@gmail.com>
2025-09-24 22:11:30 +0530

    permutations.java

---
+++ Permutations.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Permutations {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+    }
+}
